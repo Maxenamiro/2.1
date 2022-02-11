c1f686b85b8c794fc057416fc4f534a6206ddf71
Maxenamiro <maxenamiro@gmail.com>
2022-02-11 22:56:15 +0200

    6.1.2

---
+++ 6.1.2/src/com/gmail/maxenamiro/Main.java
@@ -0,0 +1,22 @@
+package com.gmail.maxenamiro;
+
+public class Main {
+	public static void main(String[] args) {
+		a(6, 13.1, "word");
+
+	}
+
+	static void a(int a, double b, String s) {
+
+		String r;
+
+		a = 23;
+		b = 6.02;
+		s = "Hello";
+		r = s + " " + b + " " + a;
+
+		System.out.print(r);
+
+	}
+
+}
