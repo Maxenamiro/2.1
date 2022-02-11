09da02cbea45d29acf13b075c32d80550fb6b428
Maxenamiro <maxenamiro@gmail.com>
2022-02-11 22:49:47 +0200

    4.1.3
    Our commit

---
+++ 4.1.3/src/com/gmail/maxenamiro/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.maxenamiro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + " * 5 = " + 5 * i);
+		}
+	}
+
+}
